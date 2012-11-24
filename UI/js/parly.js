55b42c13ba417d3edc37ab6b3f3fabab4d27ec47
Lewis Westbury <lewiswestbury@lewiswestbury-macbookair.local>
2012-11-24 18:47:45 +0000

    add result and team pages

--- UI/js/parly.js
+++ UI/js/parly.js
@@ -2,5 +2,12 @@ $(function() {
    $('.hide-on-click').click(function() {
         $(this).hide();    
    });
-    
 });
+
+function spinner(on) {
+    if (on) {
+        $('#spinner').modal('show');
+    } else {
+        $('#spinner').modal('hide');
+    }
+}
