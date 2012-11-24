a8eadc651b7f65602d6a0d513baf17da539dbf46
Lewis Westbury <lewiswestbury@lewiswestbury-macbookair.local>
2012-11-24 20:04:45 +0000

    now with logos

--- UI/js/parly.js
+++ UI/js/parly.js
@@ -1,7 +1,21 @@
 $(function() {
-   $('.hide-on-click').click(function() {
-        $(this).hide();    
-   });
+   try {
+    
+        $('.hide-on-click').click(function() {
+             $(this).hide();    
+        });
+     
+        $('.toggle-text-on-click').click(function() {
+             var currentText = $(this).text();
+             var toggleText = $(this).attr('data-text');
+             
+             $(this).text(toggleText);
+             $(this).attr('data-text', currentText);
+        });
+        
+        $('img.centered').load(function() { $(this).center(); });
+   
+   } catch(e) { alert(e); }
 });
 
 function spinner(on) {
