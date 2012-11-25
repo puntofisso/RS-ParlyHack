f24a1b6c424ffe2c8f28037cb0f1310d7896ed6b
Lewis Westbury <lewiswestbury@lewiswestbury-macbookair.local>
2012-11-25 14:24:55 +0000

    sorting results, result page

--- UI/js/parly.js
+++ UI/js/parly.js
@@ -1,3 +1,5 @@
+
+
 $(function() {
    try {
     
@@ -24,4 +26,16 @@ function spinner(on) {
     } else {
         $('#spinner').modal('hide');
     }
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
