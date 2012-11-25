4ff77f3bae886a18985025f71200bd18dfff101a
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-11-25 11:42:06 +0000

    Returns MPs by keyword

--- api/api.php
+++ api/api.php
@@ -8,25 +8,37 @@ $apicall=$_GET['api'];
 $keywords=$_GET['keywords'];
 $id = $_GET['uid'];
 
-if ($apicall=="getMP") 
+
+if ($apicall=="searchMP") 
 	$ret = searchMP($keywords);
 if ($apicall=="getMPinfo")
         $ret = queryTheyWorkForYou($id);
+if ($apicall=="getTWFYid") 
+	$ret = getTWFYid($id);
 
-
+header('Access-Control-Allow-Origin: *');
 print $ret;
 
 function searchMP($keys) {
 
+
+	$keysARR = explode(",", $keys);
+
 	$a = array();
-	for ($i = 0; $i < 5; $i++) {
-		$a[$i]["id"] = "test";
-		$a[$i]["name"] = "test";
-		$a[$i]["keywords"] = array();
-                for ($j = 0; $j < 10; $j ++) {
-			$a[$i]["keywords"][$j]["keyword"+$j] = $j;
-		}
+
+	foreach ($keysARR as $thiskey) {
+		$query = "SELECT name, keyword, sum(x) as summy, total, sum(x)/total*100 AS ratio from Keywords WHERE keyword='". $thiskey . "' group by keyword, name ORDER by ratio LIMIT 0,10";
+		$result = mySQLquery($query);
+
+		while($row = mysql_fetch_assoc( $result )) {
+         		$name = $row['name'];
+               	 	$keyword = $row['keyword'];
+               		$ratio = $row['ratio'];
+
+			$a[$name][$keyword] = $ratio;
+        	}	
 	}
+
 	return json_encode($a);
 
 }
@@ -47,8 +59,12 @@ function queryTheyWorkForYou($id) {
         // create curl resource 
         $ch = curl_init(); 
 
+	// get TWFYinfo
+	$pid =  getTWFYid($id);
+
         // set url 
-        curl_setopt($ch, CURLOPT_URL, "http://www.theyworkforyou.com/api/getMP?person_id=1567&key=CPmndLARaZ76DJLwuiB7ZqQ7"); 
+	//$query = "http://www.theyworkforyou.com/api/getMP?id=".$pid."&key=CPmndLARaZ76DJLwuiB7ZqQ7";
+        curl_setopt($ch, CURLOPT_URL, "http://www.theyworkforyou.com/api/getMP?id=$pid&key=CPmndLARaZ76DJLwuiB7ZqQ7"); 
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -63,9 +79,29 @@ function queryTheyWorkForYou($id) {
 }
 
 function getTWFYid($uid) {
-
+	$query = "SELECT NAME, MEMBER_ID, PERSON_ID from MPs WHERE MEMBER_ID=" . $uid;
+	$result = mySQLquery($query);
+	
+	while($row = mysql_fetch_assoc( $result )) {
+		$name = $row['NAME'];
+		$member_id = $row['MEMBER_ID'];
+		$person_id = $row['PERSON_ID'];
+		// This is very Dirty, but we only need one and there should only be one
+		return $person_id;
+	}
 }
 
-function mySQL()
+function mySQLquery($query) {
+mysql_connect("localhost", "rewired", "rewired") or die(mysql_error());
+mysql_select_db("RS-ParlyHack") or die(mysql_error());
+
+$result = mysql_query("$query") or die(mysql_error());  
+
+// keeps getting the next row until there are no more to get
+#while($row = mysql_fetch_array( $result )) {
+#	// Print out the contents of each row into a table
+#}
+return $result; 
+}
 
 ?>
