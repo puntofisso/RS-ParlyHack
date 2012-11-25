2d3212e5eb5ab5c377c261916e5a7907593ece3a
Lewis Westbury <lewiswestbury@lewiswestbury-macbookair.local>
2012-11-25 00:12:23 +0000

    push for localhost

--- UI/js/csv-reader.js
+++ UI/js/csv-reader.js
@@ -3,20 +3,33 @@
  * successFunction(data): data is an Array[row][col]
  */
 function retrieveCSV(fileName, successFunction) {
-    $.ajax({
-        type: "GET",
-        url: fileName,
-        dataType: "text",
-        success: function(data) { successFunction(parseCSV(data)); }
-     });
+    try {
+
+        var jqxhr = $.get(fileName, function() {
+            alert("success");
+        })
+            .success(function() { alert("second success"); })
+            .error(function() { alert("error"); })
+            .complete(function() { alert("complete"); });
+
+        /*    
+        $.get(fileName, function(data) {
+            var parsed = parseCSV(data);
+            successFunction(data);
+        });
+        */
+    
+    } catch(e) { alert(e); }
 }
 
 function parseCSV(input) {
-    var lines = input.split("\t");
-    var data = new Array();
-    for (var i = 0; i < lines.length; i++) {
-        data.push(lines[i].split(","));
-    }
-    return data;
+    try {
+        var lines = input.split("\t");
+        var data = new Array();
+        for (var i = 0; i < lines.length; i++) {
+            data.push(lines[i].split(","));
+        }
+        return data;
+    } catch (e) { alert(e); }
+    return null;
 }
-
